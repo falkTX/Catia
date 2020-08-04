6e28f8e45eb085daa3976d38a736e9b69a2e5e84
falkTX <falktx@falktx.com>
2020-08-04 10:32:26 +0100

    Install python3-dbus.mainloop.pyqt5 in CI

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo apt-get install -y \
     pylint3 \
+    python3-dbus.mainloop.pyqt5 \
     python3-pyqt5 python3-pyqt5.qtsvg python3-pyqt5.qtopengl \
     pyqt5-dev-tools \
     qtbase5-dev
